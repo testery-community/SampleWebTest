92a2a2e2488db360c696d8414e0baabbce4e3264
David Frahm <david@thefrahms.com>
2019-04-25 15:18:20 -0500

    First commit

---
+++ Web.Specs/NavigateGoogleSteps.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace Web.Specs
+{
+    [Binding]
+    public class NavigateGoogleSteps
+    {
+
+        [Given("I navigate to '(.*)'")]
+        public void GivenINavigateTo(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [Then("the page title is [\"'](.*)[\"']")]
+        public void ThePageTitleIs(string title)
+        {
+            Assert.That(driver.Title, Is.EqualTo(title));
+        }
+
+        [When("I search for '(.*)'")]
+        public void WhenISearchFor(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then("I see search results")]
+        public void ThenISeeSearchResults()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then("the environment is '(.*)'")]
+        public void TheEnvironmentIs(string expectedEnvironment)
+        {
+            Assert.That(Environment.GetEnvironmentVariable("ENVIRONMENT"), Is.EqualTo(expectedEnvironment));
+        }
+
+        [BeforeFeature("web")]
+        public static void BeforeFeature()
+        {
+            FeatureContext.Current.Add("driver", new ChromeDriver());
+            ((IWebDriver) FeatureContext.Current["driver"]).Manage().Window.Maximize();
+        }
+
+        [BeforeFeature]
+        public static void LoadEnvironment()
+        {
+            string environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+            FeatureContext.Current.Add("ENVIRONMENT", environment);
+        }
+
+        [AfterFeature("web")]
+        public static void AfterFeature()
+        {
+            ((IWebDriver)FeatureContext.Current["driver"]).Quit();
+        }
+
+        public IWebDriver driver
+        {
+            get { return ((IWebDriver)FeatureContext.Current["driver"]); }
+        }
+
+        
+    }
+}
---
+++ Web.Specs/SpecFlowFeature1.feature
@@ -0,0 +1,28 @@
+﻿@web
+Feature: Test
+
+@active
+Scenario: User Searches Yahoo
+	Given I navigate to 'http://www.yahoo.com'
+	Then the page title is 'Yahoo'
+
+@active
+Scenario: User Searches Google
+	Given I navigate to 'http://www.google.com'
+	Then the page title is 'Google'
+
+@ignore
+Scenario: User Goes to Amazon
+    Given I navigate to 'http://www.amazon.com'
+    Then the page title is "This test should not run since its missing a tag"
+
+@active
+Scenario: User Goes to Github
+	Given I navigate to 'http://www.github.com'
+	Then the page title is "The world’s leading software development platform · GitHub"
+
+@active
+Scenario: User Goes to Appveyor
+	Given I navigate to 'http://www.appveyor.com'
+	Then the page title is "Continuous Integration and Deployment service for Windows and Linux | AppVeyor"
+
